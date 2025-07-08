51eb686f738198a32668894c5e0a3c67278269a4
14priyanshu <144076158+14priyanshu@users.noreply.github.com>
2025-07-08 22:14:33 +0530

    Time: 382 ms (81.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+Select 
+  Person.firstName,
+  Person.lastName,
+  Address.city,
+  Address.state
+  From Person
+   Left Join Address
+   On  Person.personId=Address.PersonId
+  
