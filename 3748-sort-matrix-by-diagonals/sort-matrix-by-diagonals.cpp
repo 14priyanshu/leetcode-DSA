a35d7c5c6211dfe2334f559cdc118cdd9c6db3d8
14priyanshu <144076158+14priyanshu@users.noreply.github.com>
2025-08-28 20:46:02 +0530

    Time: 6 ms (76.13%) | Memory: 45.3 MB (31.07%) - LeetSync

---
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) 
+    {   int r=grid.size();
+        int c=grid[0].size();
+        map<int,vector<int>>mp;
+        for(int i=0;i<r;i++)
+            {
+                for(int j=0;j<c;j++)
+                    {
+                        mp[i-j].push_back(grid[i][j]);
+                    }
+            }
+        for(auto &it:mp)
+            {
+                if(it.first>=0)
+                 sort(it.second.begin(),it.second.end());
+                else
+                 sort(it.second.begin(),it.second.end(),greater<>());
+            }
+        for(int i=0;i<r;i++)
+            {
+                for(int j=0;j<r;j++)
+                    {
+                        grid[i][j]=mp[i-j].back();
+                        mp[i-j].pop_back();
+                    }
+            }return grid;
+
+
+        
+    }
+};
